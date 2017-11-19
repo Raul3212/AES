3331b667e0d0526003c3d6374a7637ce31cc6bf2
Raul de Araújo <rauldearaujolima@gmail.com>
2017-11-19 17:14:46 -0300

    Orientação a Objetos

--- AES.py
+++ AES.py
@@ -2,75 +2,63 @@ import numpy as np
 from util import *
 import math as mt
 import copy
-import random as rnd 
+
+mixMatrix = [
+	[2, 3, 1, 1],
+	[1, 2, 3, 1],
+	[1, 1, 2, 3],
+	[3, 1, 1, 2]
+]
 
 # Regra da multiplicação (Eq. 4.14)
 xtime = lambda a: (((a << 1) ^ 0x1B) & 0xFF) if (a & 0x80) else (a << 1)
 
-def initEstado(n):
-	estado = []
-	for i in range(n):
-		estado.append(rnd.randint(0, n-1))
-	return list2matrix(estado, int(mt.sqrt(16)))
+class AES64:
 
-def initSBox(n):
-	sBox = []
-	for i in range(n):
-		sBox.append(i)
-	return sBox
+	def __init__(self, plaintxt):
+		self.__estado = initEstado(16)
+		self.__sBox = self.__initSBox(16)
 
+	def __initSBox(self, n):
+		sBox = []
+		for i in range(n):
+			sBox.append(i)
+		return sBox
 
-def subBytes(estado, sBox):
-	print(" [>>>] SUB BYTES")
-	newEstado = copy.copy(estado)
-	for i in range(4):
-		for j in range(4):
-			newEstado[i][j] = sBox[estado[i][j]]
-	print(" [?] ESTADO:\n{}".format(np.matrix(newEstado)))
-	return newEstado
+	def __subBytes(self):
+		for i in range(4):
+			for j in range(4):
+				self.__estado[i][j] = self.__sBox[self.__estado[i][j]]
+		
+	def __shiftRows(self):
+		for i in range(len(self.__estado)):
+			self.__estado[i] = rotateList(self.__estado[i], i)
 
-def shiftRows(sBox):
-	print(" [>>>] SHIFT ROWS")
-	newEstado = []
-	for i in range(len(sBox)):
-		newEstado.append(rotateList(sBox[i], i))
-	print(" [?] ESTADO:\n{}".format(np.matrix(newEstado)))
-	return newEstado
+	def __mixColumns(self):
+		global mixMatrix
+		newEstado = [[0 for i in range(4)] for j in range(4)]
+		for i in range(4):
+			for j in range(4):
+				newEstado[i][j] = 0
+				for k in range(4):
+					if mixMatrix[i][k] == 1:
+						newEstado[i][j] ^= self.__estado[k][j]
+					elif mixMatrix[i][k] == 2:
+					 	newEstado[i][j] ^= xtime(self.__estado[k][j])
+					elif mixMatrix[i][k] == 3:
+					 	newEstado[i][j] ^= (self.__estado[k][j] ^ xtime(self.__estado[k][j]))
+		self.__estado = newEstado
 
-
-def mixColumns(estado):
-	print(" [>>>] MIX COLUMNS")
-	mixMatrix = [[2, 3, 1, 1],
-				 [1, 2, 3, 1],
-				 [1, 1, 2, 3],
-				 [3, 1, 1, 2]]
-	
-	newEstado = [[0 for i in range(4)] for j in range(4)]
-	for i in range(4):
-		for j in range(4):
-			newEstado[i][j] = 0
-			for k in range(4):
-				if mixMatrix[i][k] == 1:
-					newEstado[i][j] ^= estado[k][j]
-				elif mixMatrix[i][k] == 2:
-				 	newEstado[i][j] ^= xtime(estado[k][j])
-				elif mixMatrix[i][k] == 3:
-				 	newEstado[i][j] ^= (estado[k][j] ^ xtime(estado[k][j]))
-	print(" [?] ESTADO:\n{}".format(np.matrix(hexMatrix(newEstado))))
-	return newEstado
-
+	def runAES64(self):
+		self.__subBytes()
+		self.__shiftRows()
+		self.__mixColumns()
+		
+		print(self.__estado)
 
 def main():
-	#sBox = initSBox(16)
-	#estado = initEstado(16)
-	#print(" [?] ESTADO:\n{}".format(np.matrix(estado)))
-	#estado = subBytes(estado, sBox)
-	#estado = shiftRows(estado)
-	estado = mixColumns([[0x87, 0xf2, 0x4d, 0x97],
-						 [0x6e, 0x4c, 0x90, 0xec],
-						 [0x46, 0xe7, 0x4a, 0xc3],
-						 [0xa6, 0x8c, 0xd8, 0x95]])
-	
+	aes = AES64(None) 
+	aes.runAES64()
 
 if __name__ == '__main__':
 	main()
--- util.py
+++ util.py
@@ -1,21 +1,22 @@
+import random as rnd 
+import math as mt
+
 def list2matrix(lista, n):
 	matrix = []
 	for i in range(0, len(lista), n):
 		matrix.append(lista[i:i+n])
 	return matrix
 
+# temporária
+def initEstado(n):
+    rnd.seed(1)
+    estado = []
+    for i in range(n):
+        estado.append(rnd.randint(0, n-1))
+    return list2matrix(estado, int(mt.sqrt(n)))
+
 def rotateList(lista, n):
 	return lista[n:] + lista[0:n]
-
-def text2matrix(text):
-    matrix = []
-    for i in range(16):
-        byte = (text >> (8 * (15 - i))) & 0xFF
-        if i % 4 == 0:
-            matrix.append([byte])
-        else:
-            matrix[int(i / 4)].append(byte)
-    return matrix
 
 def hexMatrix(matrix):
 	return [[hex(x) for x in line] for line in matrix]
